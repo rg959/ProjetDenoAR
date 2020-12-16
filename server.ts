2d54865642971150e4e46a1944dfaedfc0cfca5f
alexis-savoie <alexis.savoie.555@gmail.com>
2020-12-16 13:59:43 +0100

    first commit

---
+++ server.ts
@@ -0,0 +1,15 @@
+import * as expressive from "https://raw.githubusercontent.com/NMathar/deno-express/master/mod.ts";
+
+const port = 8001;
+const app = new expressive.App();
+// route with dynamic parameter
+app.get("/", async(req, res) => {
+    await res.json([
+        { message : "Bonjour, ProjetNodeAR !" },
+    ]);
+});
+
+
+const server = await app.listen(port);
+// deno run --allow-net --allow-read --unstable server.ts
+console.log("ProjetNodeAR listening on port " + server.port);
